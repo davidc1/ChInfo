c50562a7b89573e0edd791fc5b105f02929dcacc
David <David Caratelli>
2015-03-24 11:58:38 -0500

    Adding files to ChInfo repository

---
+++ README.md
@@ -0,0 +1,63 @@
+# ChannelInfo
+
+## Dependencies
+   * numpy
+
+## Load tool
+'''
+ipython
+import getChInfo
+chinfo = getChInfo.ChanInfo()
+lchan = chinfo.getlarch(5,6,2)
+lchan.getlength()
+lchan.getnoise(0,0)
+chinfo.getlength(5,6,2)
+chinfo.getnoise(5,6,2,0,0)
+chinfo.getnoise(5,6,2,0,0,5)
+'''
+
+## Get LArSoft Channel
+Use getlarch(crate,slot,femch) function to get a "larchan" object
+a "larchan" object contains:
+- larch
+- crate
+- femch
+- larwire
+- plane
+- length [cm]
++ noise, amplitude gain and area gain arrays.
++ each array has 4 elements (one per gain setting)
++ each element is itself a 2-element array:
++ for 1 and 3 usec Shaping times.
+- noise [ [[gain=4.7 mV/fC,shaping=1 usec], [4.7,3]], [[7.8,1], [7.8,3]], [[14,1], [14,3]], [[25,1], [25,3]] ]
+- ampgain (same)
+- areagain (same)
+- gainfact (fraction of charge the channel sees from the pulser. 1 or 8/7)
+'''
+lchan = chinfo.getlarch(5,5,31)
+lchan.getlength()
+lchan.getnoise(0,0)
+lchan.getampgain(1,1)
+'''
+
+## Get Channel Information
+The "larchan" object does not need to be called directly:
+'''
+chinfo.getlength(5,5,31)
+chinfo.getampgain(5,5,31,0,0)
+'''
+
+## Cable Mapping Backwards Compatibility
+Getter functions for ChanInfo can take an optional 'run number'
+as the last argument. This will be used to map to the correct
+LArSoft channel according to how cables were mapped during
+that run.
+If no run number is specified the current (3/24/2015) cable
+positions will be used.
+Example:
+'''
+chinfo.getlength(5,5,31)
+chinfo.getlength(5,5,31,25)
+lchan = chinfo.getlarch(5,5,31)
+lchan = chinfo.getlarch(5,5,31,25)
+
---
+++ getChInfo.py
@@ -0,0 +1,210 @@
+import numpy as np
+import os,sys
+import warnings
+
+
+# A function that deterimnes the mapping version to use accoridng to run number
+# version "oldest"  (3rd dictionary in list) -> Run <= 57
+# version "old"     (2nd dictionary in list) -> Run <= 92
+# version "current" (1st dictionary in list) -> All other runs
+def getDictionaryVersion(r):
+    v = 0
+    if (r < 0):
+        raise ValueError("Run Number less than 0. Error!")
+    if (r <= 57):
+        v = 2
+    elif (r <= 92):
+        v = 1
+    else:
+        v = 0
+    return v
+
+# Defining class for LArTF Production [Crate, Slot, Femch]
+# Channel labeling
+class lartfpos:
+    def __init__(self,crate,slot,femch):
+        self.crate = crate
+        self.slot = slot
+        self.femch = femch
+    def __str__(self):
+        return "Crate %d. Slot %d. Femch %d"%(self.crate,self.slot,self.femch)
+    def __eq__(self,other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+    def __hash__(self):
+        return hash((self.crate,self.slot,self.femch))
+
+# Defining a class for the info on a LArTF Channel itself
+class larchan:
+    def __init__(self,larch,crate,femch,larwire,plane):
+        self.larch  = larch
+        self.crate = 0
+        self.femch = 0
+        self.larwire = 0
+        self.plane = 0
+        self.length = 0
+        self.noise = []
+        self.ampgain = []
+        self.areagain = []
+        self.gainfact = 0
+    # Setters
+    def setlength(self,length):
+        self.length = length
+    def setampgain(self,gain):
+        self.ampgain = gain
+    def setareagain(self,gain):
+        self.areagain = gain
+    def setnoise(self,noise):
+        self.noise = noise
+    def setgainfact(self,fact):
+        self.gainfact = fact
+    # Getters
+    def getlength(self):
+        return self.length
+    def getampgain(self,gain,shaping):
+        if ( (gain > 3) or (shaping > 1) or (gain < 0) or (shaping < 0) ):
+            print "Gains: [0 = 4.7, 1 = 7.8, 2 = 14, 3 = 25]. Shapings: [0 = 1, 1 = 3]"
+            return
+        return self.ampgain[gain][shaping]
+    def getareagain(self,gain,shaping):
+        if ( (gain > 3) or (shaping > 1) or (gain < 0) or (shaping < 0) ):
+            print "Gains: [0 = 4.7, 1 = 7.8, 2 = 14, 3 = 25]. Shapings: [0 = 1, 1 = 3]"
+            return
+        return self.areagain[gain][shaping]
+    def getgain(self,area,gain,shaping):
+        if ( (gain > 3) or (shaping > 1) or (gain < 0) or (shaping < 0) ):
+            print "Gains: [0 = 4.7, 1 = 7.8, 2 = 14, 3 = 25]. Shapings: [0 = 1, 1 = 3]"
+            return
+        if (area):
+            return self.areagain[gain][shaping]
+        return self.ampgain[gain][shaping]
+    def getnoise(self,gain,shaping):
+        if ( (gain > 3) or (shaping > 1) or (gain < 0) or (shaping < 0) ):
+            print "Gains: [0 = 4.7, 1 = 7.8, 2 = 14, 3 = 25]. Shapings: [0 = 1, 1 = 3]"
+            return
+        return self.noise[gain][shaping]
+    def getgainfact(self):
+        return self.gainfact
+
+class ChanInfo():
+    def __init__(self):
+        # Load default text files if they can be found
+        self.fname = ['','','']
+        success = 0
+        if ((os.path.isfile('fnal_map.txt'))==True):
+            self.fname[0] = 'fnal_map.txt'
+            success +=1 
+        if ((os.path.isfile('fnal_map_old.txt'))==True):
+            self.fname[1] = 'fnal_map_old.txt'
+            success += 1
+        if ((os.path.isfile('fnal_map_oldest.txt'))==True):
+            self.fname[2] = 'fnal_map_oldest.txt'
+            success += 1
+        # dictionary mapping [crate,slot,femch] -> LArSoft Channel
+        self.lardict = [{},{},{}]
+        if (success == 3):
+            self.makeDictionary()
+
+
+    def makeDictionary(self):
+        # make sure input file has been specified
+        if ( (self.fname[0] == "") or (self.fname[1] == "") or (self.fname[2] == "") ):
+            print "File name not yet specified. Cannot create dictionary"
+            return
+        if ( ((os.path.isfile(self.fname[0]))==False) or ((os.path.isfile(self.fname[1]))==False) or ((os.path.isfile(self.fname[2]))==False)):
+            print "Path to file not found."
+            return
+        # open file
+        for n in xrange(len(self.fname)):
+            infile = open(self.fname[n],'r')
+            # load file contents to numpy array
+            ch = np.loadtxt(infile,delimiter=' ',
+                            dtype={'names':('crate','slot','femch','larch','larwire','plane','length',
+                                            'rms01','rms03','rms11','rms13','rms21','rms23','rms31','rms33',
+                                            'amp01','amp03','amp11','amp13','amp21','amp23','amp31','amp33',
+                                            'area01','area03','area11','area13','area21','area23','area31','area33',
+                                            'gainnorm'),
+                                   'formats':('u1','u1','u2','u2','u2','u1','f3',
+                                              'f2','f2','f2','f2','f2','f2','f2','f2',
+                                              'f2','f2','f2','f2','f2','f2','f2','f2',
+                                              'f2','f2','f2','f2','f2','f2','f2','f2',
+                                              'f2')})
+            # loop over all entry and populate the dictionaries
+            for i in xrange(len(ch)):
+                # Set [crate,slot,femch] position for dictonary
+                thischan = lartfpos(ch['crate'][i],ch['slot'][i],ch['femch'][i])
+                # now create larsoft wire object
+                thislar = larchan(ch['larch'][i],ch['crate'][i],ch['femch'][i],ch['larwire'][i],ch['plane'][i])
+                thislar.setlength(ch['length'][i])
+                thisnoise = [ [ch['rms01'][i],ch['rms03'][i]], [ch['rms11'][i],ch['rms13'][i]], [ch['rms21'][i],ch['rms23'][i]], [ch['rms31'][i],ch['rms33'][i]] ]
+                thisamp   = [ [ch['amp01'][i],ch['amp03'][i]], [ch['amp11'][i],ch['amp13'][i]], [ch['amp21'][i],ch['amp23'][i]], [ch['amp31'][i],ch['amp33'][i]] ]
+                thisarea  = [ [ch['area01'][i],ch['area03'][i]], [ch['area11'][i],ch['area13'][i]], [ch['area21'][i],ch['area23'][i]], [ch['area31'][i],ch['area33'][i]] ]
+                thislar.setnoise(thisnoise)
+                thislar.setampgain(thisamp)
+                thislar.setareagain(thisarea)
+                thislar.setgainfact(ch['gainnorm'][i])
+                # Add to dictionary
+                self.lardict[n][thischan] = thislar
+            del ch
+            infile.close()
+        print "done filling dictionary!"
+
+
+    def isinputvalid(self,crate,slot,femch,r):
+        # First check that the run number exists
+        v = 0
+        try:
+            v = getDictionaryVersion(r)
+        except ValueError as detail:
+            raise ValueError(detail)
+        # now check that this [crate,slot,femch] maps to a larchannel
+        x = lartfpos(crate,slot,femch)
+        if ( (x in self.lardict[v]) == False):
+            raise ValueError("[Crate,Slot,Femch] Not found in Dictionary")
+        # If all is good, return the larch object
+        return x,v
+        
+
+    def getlarch(self,crate,slot,femch,r=100):
+        try:
+            x,v = self.isinputvalid(crate,slot,femch,r)
+            return self.lardict[v][x]
+        except ValueError as detail:
+            print "Error: ",detail
+        
+    def getlength(self,crate,slot,femch,r=100):
+        try:
+            x,v = self.isinputvalid(crate,slot,femch,r)
+            return self.lardict[v][x].getlength()
+        except ValueError as detail:
+            print "Error: ",detail
+
+    def getnoise(self,crate,slot,femch,gain,shaping,r=100):
+        try:
+            x,v = self.isinputvalid(crate,slot,femch,r)
+            return self.lardict[v][x].getnoise(gain,shaping)
+        except ValueError as detail:
+            print "Error: ",detail
+
+    def getampgain(self,crate,slot,femch,gain,shaping,r=100):
+        try:
+            x,v = self.isinputvalid(crate,slot,femch,r)
+            return self.lardict[v][x].getampgain(gain,shaping)
+        except ValueError as detail:
+            print "Error: ",detail
+
+    def getareagain(self,crate,slot,femch,gain,shaping,r=100):
+        try:
+            x,v = self.isinputvalid(crate,slot,femch,r)
+            return self.lardict[v][x].getareagain(gain,shaping)
+        except ValueError as detail:
+            print "Error: ",detail
+
+    def getgainfact(self,crate,slot,femch,r=100):
+        try:
+            x,v = self.isinputvalid(crate,slot,femch,r)
+            return self.lardict[v][x].getgainfact()
+        except ValueError as detail:
+            print "Error: ",detail
